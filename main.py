bc0996175e2eacd30b0c658c7bf494dad2115240
MikkyOdey <michaelodey12m@gmail.com>
2026-08-28 17:18:30 +0100

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import Calculator
-
-
-print("Addition:", Calculator.add(3, 5))
-print("Subtraction:", Calculator.subtract(3, 5))
-print("Multiplication:", Calculator.multiply(3, 5))
-print("Division:", Calculator.divide(3, 5))
